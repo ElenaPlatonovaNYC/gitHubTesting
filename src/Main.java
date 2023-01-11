4245016d77a2a561b3ffee49025cdc83d4ed375e
fiestalifeofelena <fiesta@MacBook-Air-FIESTA.local>
2023-01-11 16:48:38 -0500

    version1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Elena Platonova is good QA ingeneer");
+        System.out.println("I need more help so Im looking for tutor");
+
+
+
+    }
+}
